27d0dd3c195c33f7df54049e4ec656e018766207
ncerice <nacerice@crimson.ua.edu>
2021-06-14 19:56:38 -0500

    added registration form elements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './style.css';
 import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Col from 'react-bootstrap/Col';
 import RegistrationPage from '../../components/RegistrationPage';
 
 export default function App() {
   return (
-    <Container className="mainContainer">
-      <RegistrationPage />
-    </Container>
+    <Jumbotron>
+      <Container className="main-content">
+        <Col md={{ span: 8, offset: 2 }}>
+          <RegistrationPage />
+        </Col>
+      </Container>
+    </Jumbotron>
   );
 }
